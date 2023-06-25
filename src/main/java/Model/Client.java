dd429fff1569413fadcca02f25f168c7e9ee3f4d
tklin001 <tim.klingelhoefer@student.hs-rm.de>
2023-06-26 01:00:23 +0200

    updated client & server in master

--- src/main/java/Model/Client.java
+++ src/main/java/Model/Client.java
@@ -1,5 +1,7 @@
 package Model;
 
+import javafx.scene.chart.PieChart;
+
 import java.io.*;
 import java.net.Socket;
 import java.io.IOException;
@@ -10,18 +12,31 @@ import java.net.SocketTimeoutException;
 
 
 public class Client {
-    static Socket socket;
+    DatagramSocket udpSocket;
+
+
+    byte buffer[] = new byte[65000];
+    Socket socket;
     int port;
+
     String address;
-    
+    InetAddress partnerAddress;
+    InetAddress packetAddress;
+    int packetPort;
+    boolean requestReceived = false;
     String username;
     String password;
 
-    Client(int port, String address){
+    String serverMesage;
+
+    public Client(int port, String address){
+
+
         this.port = port;
         this.address = address;
         try {
             socket = new Socket(address,port);
+
         }catch(IOException e){
             e.printStackTrace();
         }
@@ -29,8 +44,115 @@ public class Client {
 
     }
 
+    public void startChatProtocol(String port, String ip){
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String msg;
+        boolean running = true;
+        try {
+            while(running) {
+
+                partnerAddress = InetAddress.getByName(ip.replace("/","").strip());
+
+                udpSocket = new DatagramSocket();
+
+                msg= "--connection established--";
+                System.out.println(msg);
+                buffer = msg.getBytes();
+
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, partnerAddress, Integer.parseInt(port));
+                udpSocket.send(packet);
+                startChatProtocol();
+                running = false;
+            }
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+    public void startChatProtocol(){
+        buffer = new byte[65000];
+
+        Thread receiveMessages = new Thread(this::receiveMessage);
+        receiveMessages.start();
+
+
+        InetAddress address;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String msg = username + ": ";
+        try {
+            while (true) {
+                //System.out.println();
+                msg = username + ": " +bufferedReader.readLine();
+
+                System.out.println("                 " + msg);
+                sendMessage(msg);
+
+
+            }
+        }catch(Exception e) {
+
+        }
+    }
+
+    public void receiveMessage(){
+        try{
+            while(true) {
+
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                udpSocket.receive(packet);
+                packetPort= packet.getPort();
+                packetAddress = packet.getAddress();
+
+                String received = new String(packet.getData(), 0, packet.getLength());
+                Thread.sleep(2000);
+                System.out.println(received);
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+    public void sendMessage(String msg){
+        byte[] messageBytes;
+
+        messageBytes = msg.getBytes();
+        try{
+            int maxPacketSize = udpSocket.getSendBufferSize()- 4;
+            int totalPackets = (int) Math.ceil((double) messageBytes.length / maxPacketSize);
+            System.out.println("TOTAL PACKETS: " + totalPackets + " MAXPACKETSIZE: " + maxPacketSize + " MESSAGEBYTES: "+ messageBytes);
+            buffer = Integer.toString(totalPackets).getBytes();
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, packetAddress, packetPort);
+            udpSocket.send(packet);
+
+            //buffer = msg.getBytes();
+
+            for(int i = 0;i < totalPackets; i++) {
+                System.out.println(i);
+                int offset = i*maxPacketSize;
+                int length = Math.min(maxPacketSize, messageBytes.length - offset);
+                buffer = new byte[length];
+                System.arraycopy(messageBytes, offset, buffer, 0, length);
+
+                packet = new DatagramPacket(buffer, buffer.length, packetAddress, packetPort);
+                udpSocket.send(packet);
+
+
+            }
+        }
+        catch (Exception e) {
+
+        }
+
+
+
+    }
+
     public static void main(String args[]){
-        Client client = new Client(5556,"localhost");
+        Client client = new Client(25655,"localhost");
         try{
 
             client.clientLoop();
@@ -43,6 +165,7 @@ public class Client {
 
     }
     public void clientLoop() throws IOException{
+
         DataOutputStream dataOut = new DataOutputStream(socket.getOutputStream());
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
@@ -58,21 +181,26 @@ public class Client {
 
             String userData = username + ";" + password;
             dataOut.writeUTF(userData);
-            while (true) {
-                if (!registerd) {
 
+
+            if (!registerd) {
+                while(!registerd){
                     switch (leseNachricht(socket).strip()) {
                         case "REG_OK":
                             System.out.println("Du bist mit dem Server verbunden!");
-                            System.out.println("Hier ist eine Liste mit clients die online sind!");
+
                             registerd = true;
+                            break;
+
 
                         case "PASSWORD_INVALID":
                             System.out.println("Passwort falsch bitte neu versuchen!");
                             password = bufferedReader.readLine();
                             userData = username + ";" + password;
                             dataOut.writeUTF(userData);
+                            System.out.println(userData);
                             break;
+
 
                         case "REG_NEW":
                             System.out.println("Du wurdest als neue Benutzer angemeldet");
@@ -80,18 +208,112 @@ public class Client {
 
 
                     }
-                } else if (registerd) {
-                    String connectedClients = leseNachricht(socket).strip();
-                    System.out.println(connectedClients);
-                    System.out.println("Mit welchem Client möchtests du verbinden");
                 }
+            }
+            if (registerd) {
+
+
+                Thread serverMessageHandler = new Thread(() -> {
+
+                    try {
+                        while (true) {
+
+                            serverMesage = leseNachricht(socket).strip();
+                            System.out.println("RECEIVED FROM SERVER: " + serverMesage);
+                            switch (serverMesage) {
+                                case "REQUEST_ACCEPTED":
+                                    System.out.println("starting chat!");
+                                    serverMesage = leseNachricht(socket);
+                                    String chatPartnerPort = serverMesage.split(";")[0];
+                                    String chatPartnerIP = serverMesage.split(";")[1];
+
+                                    startChatProtocol(chatPartnerPort,chatPartnerIP);
+                                    break;
+
+
+                                case "CHAT_REQUEST":
+                                    System.out.println("Chat Einladung erhalten! y: zum akzeptieren ODER n: zum ablehnen!");
+                                    requestReceived = true;
+
+                                    break;
+
+                                case "USER_NOT_FOUND":
+                                    System.out.println("Benutzer nicht gefunden, bitte erneut eingeben!");
+
+                                case "REQUEST_DENIED":
+                                    System.out.println("ABGELEHNT");
+
+                                default:
+                                    System.out.println(serverMesage);
+                            }
+                        }
+                    }
+                    catch(IOException e){
+                        e.printStackTrace();
+                    }
+                });
+
+                Thread userInputHandler = new Thread(() -> {
+
+                    try {
+                        while (true) {
+                            String eingabe[] = bufferedReader.readLine().split(" ");
+                            //System.out.println("UR INPUT: "+ eingabe);
+                            switch (eingabe[0]) {
+                                case "!clients":
+                                    dataOut.writeUTF("GET_CLIENTS");
+                                    //System.out.println(serverMesage);
+                                    break;
+
+                                case "!connect":
+                                    //System.out.println("UR INPUT: "+ eingabe[0]);
+                                    dataOut.writeUTF(eingabe[1]);
+
+                                    break;
+
+                                case "y":
+                                    if (requestReceived) {
+                                        udpSocket = new DatagramSocket();
+                                        dataOut.writeUTF("REQUEST_ACCEPTED "+ udpSocket.getLocalPort());
+                                        System.out.println("Starting chat!");
+                                        startChatProtocol();
+                                    }
+                                    break;
+
+                                case "n":
+                                    if (requestReceived) {
+                                        dataOut.writeUTF("REQUEST_DENIED");
+                                    }
+                                    break;
+
+                                default:
+                                    //System.out.println("USER INPUT:" + eingabe);
+                            }
+                            //System.out.println("end");
+                        }
+                    }
+                    catch(IOException e){
+                        e.printStackTrace();
+                    }catch(ArrayIndexOutOfBoundsException e){
+                        System.out.print("Gib ein client ein zu dem du connecten willst!");
+                    }
+                });
+
+                userInputHandler.start();
+                serverMessageHandler.start();
+
+
 
 
             }
+
+
+
+
+
         }catch(IOException e){
             e.printStackTrace();
         }
-
 
 
     }
--- src/main/java/Model/MultiServer.java
+++ src/main/java/Model/MultiServer.java
@@ -3,12 +3,19 @@ package Model;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class MultiServer {
 
-    HashMap connected = new HashMap();
+    // HashMap<String, Integer> connected = new HashMap();
+    Socket wantedSocket;
+    Socket requestUser;
+    Map<String, ClientHandler> usernameClientMap = new HashMap<>();
+    List<ClientHandler> clients = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         MultiServer multiServer = new MultiServer();
@@ -21,11 +28,14 @@ public class MultiServer {
 
         ServerSocket serverSocket = new ServerSocket(port);
 
+
         while(true){
             Socket client = warteAufAnmeldung(serverSocket);
 
-            Thread clientThread = new ClientHandler(client);
-            clientThread.start();
+            //Thread clientThread = new ClientHandler(client, connected, clientSockets);
+            ClientHandler clientHandler = new ClientHandler(client);
+            clients.add(clientHandler);
+            clientHandler.start();
         }
 
 
@@ -39,41 +49,54 @@ public class MultiServer {
     Socket warteAufAnmeldung(ServerSocket serverSocket) throws IOException{
         Socket socket = serverSocket.accept();
 
-        System.out.print("connected");
+
+        System.out.print(socket.getInetAddress().toString()+ " "+ socket.getPort() + " connected");
         return socket;
     }
 
 
 
-    static class ClientHandler extends Thread{
+    class ClientHandler extends Thread{
         Socket clientSocket;
-        int count = 0;
+
         boolean isInList = false;
         String userData;
-
         String username;
         String password;
         String line;
-        String nachricht;
+        String nachricht = null;
+        List<Socket> clientSockets;
+        String clientsConnected = "";
 
+
+        public int i =0;
         public ClientHandler(Socket clientSocket) throws IOException {
+
             this.clientSocket = clientSocket;
+            //this.clientSockets  = sockets;
+            //clientSockets.add(clientSocket);
+            //System.out.println(clientSockets);
+            i++;
 
         }
 
+        @Override
         public void run(){
+            int count = 0;
+
             try{
                 DataInputStream dataIn = new DataInputStream(clientSocket.getInputStream());
-                BufferedReader reader = new BufferedReader(new FileReader("src/main/java/resources/userData.csv"));
-                BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/java/resources/userData.csv", true));
+                BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/Accounts.csv"));
+                BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/Accounts.csv", true));
 
-                while (true) {
+                while(true) {
                     nachricht = dataIn.readUTF();
-
+                    System.out.println("CLIENT MESSAGE: " + nachricht);
                     switch (count) {
                         case 0 -> {
                             userData = nachricht;
                             username = userData.split(";")[0];
+                            password = userData.split(";")[1];
                             while ((line = reader.readLine()) != null) {
                                 if (username.equals(line.split(";")[0])) {
                                     isInList = true;
@@ -81,33 +104,86 @@ public class MultiServer {
                                 }
                             }
                             if (!isInList) {
-                                writer.write(username + ";");
-                            }
-                            count++;
-                        }
-                        case 1 -> {
-                            password = userData.split(";")[1];
-                            if (!isInList) {
-                                writer.write(password + "\n");
+                                writer.write(username + ";" + password + "\n");
+                                System.out.println("gesendet regnew"+ username + password);
                                 schreibeNachricht(clientSocket, "REG_NEW");
-                                count++;
 
+                                writer.close();
 
-                            } else {
+                                usernameClientMap.put(username, this);
+
+                            }else{
                                 if (password.equals(line.split(";")[1])) {
                                     schreibeNachricht(clientSocket, "REG_OK");
-
+                                    writer.close();
+                                    usernameClientMap.put(username, this);
                                     count++;
+
+                                }else{
+
+                                    reader = new BufferedReader(new FileReader("src/main/resources/Accounts.csv"));
+                                    schreibeNachricht(clientSocket, "PASSWORD_INVALID");
+
+
                                 }
-                                else schreibeNachricht(clientSocket, "PASSWORD_INVALID");
                             }
-                            writer.close();
+
+
+
+
+                        } case 2 ->{
+
+
+                            if(clientsConnected.contains(nachricht)){
+
+                                wantedSocket = usernameClientMap.get(nachricht).clientSocket;
+                                requestUser = clientSocket;
+
+                                schreibeNachricht(wantedSocket, "CHAT_REQUEST");
+                                System.out.println("SEND  REQUEST");
+                                count++;
+
+                            }else{
+                                schreibeNachricht(clientSocket, "USER_NOT_FOUND");
+                            }
+
+                        } case 3 ->{
+
+
+
+
                         }
-                        default -> {
+                        default ->{
+                            if(nachricht.equals("GET_CLIENTS")) {
+                                for (Object i : usernameClientMap.keySet()) {
+                                    if (i != null) clientsConnected += i + " ";
+
+
+                                }
+                                schreibeNachricht(clientSocket, clientsConnected);
+                                count = 2;
+                                System.out.println("default");
+                            }
+
+
+
+                            if(nachricht.split(" ")[0].equals("REQUEST_ACCEPTED")){
+                                schreibeNachricht(requestUser, "REQUEST_ACCEPTED");
+                                schreibeNachricht(requestUser,nachricht.split(" ")[1]+ ";" + wantedSocket.getInetAddress());
+
+                            }else if(nachricht.equals("REQUEST_DENIED")){
+
+                                schreibeNachricht(requestUser,"REQUEST_DENIED");
+
+                            }
+
 
                         }
                     }
-                    System.out.println(nachricht);
+
+
+
+                    System.out.println(count);
                     if (nachricht.equalsIgnoreCase("exit")) {
                         clientSocket.close();
                         System.out.println("Disconnected from the server");
@@ -115,11 +191,11 @@ public class MultiServer {
                     }
 
 
-                    System.out.println();
+
                 }
 
             }catch(IOException e){
-
+                e.printStackTrace();
             }
         }
 
@@ -131,6 +207,6 @@ public class MultiServer {
         }
 
     }
-    
-    
+
+
 }
